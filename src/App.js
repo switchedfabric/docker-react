69f18f9add3c101463b8a1dc4fd8c3106da18013
Willem Roos <wroos@shoprite.co.za>
2019-12-13 14:57:08 +0200

    App.js change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hello there world, to get started, edit <code>src/App.js</code> and save to reload.
+          Hello there world again, to get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
